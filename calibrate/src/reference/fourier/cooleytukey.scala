5044c310184fc2d17ed6ff40a6edbedee656a9a7
Yi Lee <ylee1228@umd.edu>
2025-02-09 10:13:26 +0000

    Implement Cooley-Tukey IFFT in software

--- calibrate/src/reference/fourier/cooleytukey.scala
+++ calibrate/src/reference/fourier/cooleytukey.scala
@@ -8,6 +8,10 @@ class Precompute(val num_layers : Int) {
   def twiddle_factor(l : Int, j : Int) : Complex[Double] = {
     return Complex.polar(1.0, - 2 * Pi * j / (1 << (num_layers - l)))
   }
+
+  def inv_twiddle_factor(l : Int, j : Int) : Complex[Double] = {
+    return Complex.polar(1.0, 2 * Pi * j / (1 << (num_layers - l)))
+  }
 }
 
 // Result index is bit-reversed
@@ -42,7 +46,43 @@ class CooleyTukey(xs : Seq[Complex[Double]]) {
 
   def compute() : Seq[Complex[Double]] = {
     for(_ <- 0 until num_layers) {
-      println(content)
+      iter_layer()
+    }
+    return content
+  }
+}
+
+class InvCooleyTukey(xs : Seq[Complex[Double]]) {
+  val n = xs.length
+  val num_layers = 31 - Integer.numberOfLeadingZeros(n)
+  val precompute = new Precompute(num_layers)
+
+  var content = xs
+  var layer = num_layers - 1
+
+  def iter_layer() = {
+    val group_size = n >> layer
+    val num_groups = 1 << layer
+    val new_content = new Array[Complex[Double]](n)
+    for(g <- 0 until num_groups) {
+      for(j <- 0 until (group_size / 2)) {
+        val left = g * group_size + j
+        val right = left + group_size / 2
+        val vL = content(left)
+        val vR = content(right)
+        val w = precompute.inv_twiddle_factor(layer, j)
+        val aL = (vL + w * vR) / 2
+        val aR = (vL - w * vR) / 2
+        new_content(left) = aL
+        new_content(right) = aR
+      }
+    }
+    content = new_content
+    layer = layer - 1
+  }
+
+  def compute() : Seq[Complex[Double]] = {
+    for(_ <- 0 until num_layers) {
       iter_layer()
     }
     return content
@@ -60,9 +100,11 @@ object TestCooleyTukey extends App {
   Complex(-7.89, -0.12),
   Complex(3.45, -6.78)
   )
+  println(data)
   val ct = new CooleyTukey(data)
-  println(ct.compute())
-
-  val fft = new FourierTransform(data)
-  println(fft.compute())
+  val res = ct.compute()
+  println(res)
+  val inv_ct = new InvCooleyTukey(res)
+  val inv_res = inv_ct.compute()
+  println(inv_res)
 }
