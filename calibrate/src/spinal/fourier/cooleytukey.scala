e5486c2fbb70f25c04e08c04222261e1cd596f7f
Yi Lee <ylee1228@umd.edu>
2025-02-17 17:46:30 -0500

    Implement Cooley-Tukey in hardware

---
+++ calibrate/src/spinal/fourier/cooleytukey.scala
@@ -0,0 +1,85 @@
+package fourier
+
+import complex._
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib._
+import reference.fourier.Precompute
+import reference.fourier.FourierTransform
+import spire.math.Complex
+
+class CooleyTukey(xslen : Int) extends Component {
+  val precompute = new Precompute(xslen)
+  val xs = in(Vec.fill(xslen)(Cx()))
+  val ys = out(Vec.fill(xslen)(Cx()))
+
+  var content = xs
+  var layer = 0
+
+  def iter_layer() = {
+    val group_size = xslen >> layer
+    val num_groups = 1 << layer
+    var new_content = Vec.fill(xslen)(Cx())
+    for(g <- 0 until num_groups) {
+      for(j <- 0 until (group_size / 2)) {
+        val left = g * group_size + j
+        val right = left + group_size / 2
+        val vL = content(left)
+        val vR = content(right)
+       
+        val addL = new CxAdd()
+        addL.z1 := vL
+        addL.z2 := vR
+        val aL = addL.s
+
+        val subR = new CxSub()
+        subR.z1 := vL
+        subR.z2 := vR
+        val twiddle_factor = precompute.twiddle_factor(layer, j)
+        val mulR = new CxMul()
+        mulR.z1 := subR.d
+        mulR.z2.setval(twiddle_factor)
+        val aR = mulR.prod
+
+        new_content(left) := aL
+        new_content(right) := aR
+      }
+    }
+    content = new_content
+    layer = layer + 1
+  }
+  
+  for(_ <- 0 until precompute.num_layers) {
+    iter_layer()
+  }
+
+  for(i <- 0 until xs.length) {
+    ys(i) := content(i)
+  }
+}
+
+object TestCooleyTukey extends App {
+  val data = Seq(
+  Complex(1.23, -4.56),
+  Complex(7.89, 0.12),
+  Complex(-3.45, 6.78),
+  Complex(9.01, -2.34),
+  Complex(-5.67, 8.90),
+  Complex(1.23, 4.56),
+  Complex(-7.89, -0.12),
+  Complex(3.45, -6.78)
+  )
+  val ref_res = (new reference.fourier.CooleyTukey(data)).compute()
+  println(f"correct result = $ref_res")
+  SimConfig.compile { new CooleyTukey(data.length) }.doSim { dut =>
+    for(i <- 0 until 8) {
+      dut.xs(i).re #= data(i).real
+      dut.xs(i).im #= data(i).imag
+    }
+    sleep(1)
+    val res = Seq.tabulate(8) { i =>
+      Complex(dut.ys(i).re.toDouble, dut.ys(i).im.toDouble)
+    }
+    println(f"computed result = $res")
+  }
+}
--- calibrate/src/spinal/misc/complex.scala
+++ calibrate/src/spinal/misc/complex.scala
@@ -1,12 +1,34 @@
-package misc
+package complex
 
 import spinal.core._
 import spinal.core.sim._
 import spinal.lib._
+import spire.math.Complex
 
 case class Cx() extends Bundle {
-  val re = AFix.S(8 exp, -24 exp)
-  val im = AFix.S(8 exp, -24 exp)
+  val re = SFix(8 exp, -24 exp)
+  val im = SFix(8 exp, -24 exp)
+
+  def setval(z: Complex[Double]) {
+    re := z.real
+    im := z.imag
+  }
+}
+
+class CxAdd extends Component {
+  val z1 = in(Cx())
+  val z2 = in(Cx())
+  val s = out(Cx())
+  s.re := (z1.re + z2.re).truncated
+  s.im := (z1.im + z2.im).truncated
+}
+
+class CxSub extends Component {
+  val z1 = in(Cx())
+  val z2 = in(Cx())
+  val d = out(Cx())
+  d.re := (z1.re - z2.re).truncated
+  d.im := (z1.im - z2.im).truncated
 }
 
 class CxMul extends Component {
@@ -20,9 +42,9 @@ class CxMul extends Component {
 object TestComplex extends App {
   SimConfig.compile{ new CxMul }.doSim { dut =>
     dut.z1.re #= 0.1
-    dut.z1.im #= 0.1
-    dut.z2.re #= 0.1
-    dut.z2.im #= 0.1
+    dut.z1.im #= -0.2
+    dut.z2.re #= 0.3
+    dut.z2.im #= 0.4
     sleep(1)
     println(dut.prod.re.toDouble)
     println(dut.prod.im.toDouble)
