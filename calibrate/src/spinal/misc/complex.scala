ee424da438b914cd4d44759c05f5aa11dba2c99c
Yi Lee <ylee1228@umd.edu>
2025-02-18 13:43:48 -0500

    Write helper circuit to sum Cx Vec

--- calibrate/src/spinal/misc/complex.scala
+++ calibrate/src/spinal/misc/complex.scala
@@ -4,6 +4,7 @@ import spinal.core._
 import spinal.core.sim._
 import spinal.lib._
 import spire.math.Complex
+import spire.implicits._
 
 case class Cx() extends Bundle {
   val re = SFix(16 exp, -16 exp)
@@ -46,12 +47,47 @@ class CxDiv2 extends Component {
   z2.im := (z.im >> 1).truncated
 }
 
+class CxVecAdd(n: Int) extends Component {
+  val zs = in(Vec.fill(n)(Cx()))
+  val s = out(Cx())
+  
+  var terms = zs
+  while(terms.size != 1) {
+    val new_sz = (terms.size + 1) / 2
+    val next_terms = Vec.fill(new_sz)(Cx())
+    for(j <- 0 until new_sz) {
+      if(2 * j + 1 != terms.size) {
+        next_terms(j).re := terms(2 * j).re + terms(2 * j + 1).re
+        next_terms(j).im := terms(2 * j).im + terms(2 * j + 1).im
+      } else {
+        next_terms(j) := terms(2 * j)
+      }
+    }
+    terms = next_terms
+  }
+  s := terms(0)
+}
+
 object TestComplex extends App {
-  SimConfig.compile{ new CxDiv2 }.doSim { dut =>
-    dut.z.re #= 1.1
-    dut.z.im #= -2.2
+  val n = scala.util.Random.nextInt(8) + 3
+  def randc() : Complex[Double] = {
+    val re = -2 + 4 * scala.util.Random.nextDouble()
+    val im = -2 + 4 * scala.util.Random.nextDouble()
+    return Complex(re, im)
+  }
+  val arr = Seq.fill(n)(randc())
+  val s = arr.reduce(_ + _)
+
+  println(f"arr = $arr")
+  println(f"sum = $s")
+
+  SimConfig.compile{ new CxVecAdd(n) }.doSim { dut =>
+    for(i <- 0 until n) {
+      dut.zs(i).re #= arr(i).real
+      dut.zs(i).im #= arr(i).imag
+    }
     sleep(1)
-    println(dut.z2.re.toDouble)
-    println(dut.z2.im.toDouble)
+    println(dut.s.re.toDouble)
+    println(dut.s.im.toDouble)
   }
 }
